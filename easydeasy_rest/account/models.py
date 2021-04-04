259ccf44c93e2481fc5ac655e36a666a23c8aa9a
Paz <yolobazak1@gmail.com>
2021-04-04 14:53:17 +0300

    Added docs. small modification to the Account model.

--- easydeasy_rest/account/models.py
+++ easydeasy_rest/account/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, AbstractUser
+from easydeasy_rest.api.models import Disease, Article
 
 
 class MyAccountManager(BaseUserManager):
+    """
+    This is a custom account manager which is responsible for managing creating of new accounts models.
+
+    We manage the creation of normal and super user here!
+    """
 
     def create_user(self, email, username, password=None):
+        """
+        Creating normal users
+        :return: The new created user
+        """
 
         if not email:
             raise ValueError('User must have email!')
@@ -21,7 +31,10 @@ class MyAccountManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, username, password):
-
+        """
+        Creating super user
+        :return: The new created super user
+        """
         user = self.model(
             email=self.normalize_email(email),
             username=username
@@ -37,24 +50,13 @@ class MyAccountManager(BaseUserManager):
         return user
 
 
-def get_profile_image_path(self) -> str:
-    """
-    Returns the path for the account profile image.
-    :param self: Account
-    :return: Path to it's account profile image
-    """
-    return f'profile_images/{self.pk}/{"profile_image.png"}'
-
-
-def get_default_profile_image_path() -> str:
-    """
-    Returns the path for the default profile image.
-    :return: Path to it's account profile image
-    """
-    return f'default_profile_image/default_profile_image.png'
-
-
 class Account(AbstractBaseUser):
+    """
+    This model is representing our custom account user model.
+
+    *IMPORTANT*
+    This user is authenticated with Email & Password.
+    """
 
     email = models.EmailField(verbose_name='email', max_length=64, unique=True)
     username = models.CharField(max_length=32)
@@ -66,17 +68,16 @@ class Account(AbstractBaseUser):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
-    profile_image = models.ImageField(max_length=255, upload_to=get_profile_image_path, null=True, blank=True,
-                                      default=get_default_profile_image_path)
-
     objects = MyAccountManager()
 
     USERNAME_FIELD = 'email'
 
     REQUIRED_FIELDS = ['username']
 
-    def get_profile_image_filename(self):
-        return str(self.profile_image)[str(self.profile_image).index(f'profile_images/{self.pk}/'):]
+    # Our own custom fields
+    has_agreed_to_terms = models.BooleanField(default=True)
+    followed_diseases = models.ManyToManyField(Disease, blank=True)
+    saved_articles = models.ManyToManyField(Article, blank=True)
 
     def has_perm(self, perm, obj=None):
         return self.is_admin
