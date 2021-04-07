e798ce448543945865db02cc120f93439068f9c6
milimetron <54635781+milimetron@users.noreply.github.com>
2021-04-07 19:24:07 +0300

    Added latest Article Feature (#5)
    * Added latest Article Feature
    
    * Removed check for no page

--- easydeasy_rest/api/views.py
+++ easydeasy_rest/api/views.py
@@ -1,3 +1,6 @@
+from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from .models import Category, DefaultCategoryImage, Feed, Site, Disease, Article
 from .serializers import DefaultCategoryImageSerializer, SiteSerializer, DiseaseSerializer, ArticleSerializer, \
@@ -12,6 +15,29 @@ class SiteViewSet(ModelViewSet):
 class DiseaseViewSet(ModelViewSet):
     queryset = Disease.objects.all()
     serializer_class = DiseaseSerializer
+
+    @action(detail=False, url_path='(?P<url_disease_id>[0-9]+)/latest_articles')
+    def latest_disease_articles(self, request, url_disease_id):
+        """ returns the list of newest articles.
+
+            (?P<url_disease_id>[0-9]+) in the decorator forces to only receive ints for url_disease_id.
+            """
+
+        # Converts string url to int
+        disease_id = int(url_disease_id)
+
+        article_queryset = Article.objects.filter(diseases__id=disease_id)
+
+        # A pagination class used specifically to page the latest articles based on there dates
+        class LatestArticlesSetPagination(PageNumberPagination):
+            page_size = 10
+
+        pagination = LatestArticlesSetPagination()
+
+        page = pagination.paginate_queryset(article_queryset, request)
+
+        serializer = ArticleSerializer(page, many=True, context={'request': request})
+        return pagination.get_paginated_response(serializer.data)
 
 
 class ArticleViewSet(ModelViewSet):
