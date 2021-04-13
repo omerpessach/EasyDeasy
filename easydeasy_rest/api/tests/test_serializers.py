85d189bcf62e97fff013ba5089c5a00559c55c69
Paz <yolobazak1@gmail.com>
2021-04-13 17:06:05 +0300

    Fixed plate + Added FeedTest

--- easydeasy_rest/api/tests/test_serializers.py
+++ easydeasy_rest/api/tests/test_serializers.py
@@ -17,7 +17,6 @@ class CategorySerializersTests(APITestCase):
         self.category_data = {'name': 'category_name'}
 
         self.category = Category.objects.create(**self.category_data)
-        self.serializer = CategorySerializer(instance=self.category)
 
     def test_contains_expected_fields(self):
         self.assertCountEqual(set(self.category_data.keys()), {'name'})
@@ -29,11 +28,42 @@ class CategorySerializersTests(APITestCase):
         self.assertTrue(serializer.is_valid())
 
     def test_display_serializer(self):
-        display_serializer = CategorySerializer(Category.objects.get(name='category_name'))
+        display_serializer = CategorySerializer(self.category)
 
         category_data = display_serializer.data
 
         self.assertEquals(category_data['name'], self.category_data['name'])
+
+
+class FeedSerializersTests(APITestCase):
+
+    def setUp(self):
+        self.source_site = Site.objects.create(name='site', url='www.mysite.com')
+        self.feed_data = {'url': 'www.site/feeds/my-feed.com',
+                          'missing_fields': 'I',
+                          'update_time': 10,
+                          'source_site': self.source_site.pk}
+
+        self.feed = Feed.objects.create(**self.feed_data | {'source_site': self.source_site})
+
+    def test_contains_expected_fields(self):
+        self.assertCountEqual(set(self.feed_data.keys()), {'url', 'missing_fields', 'update_time', 'source_site'})
+
+    def test_valid(self):
+        serializer = FeedSerializer(data=self.feed_data)
+
+        # Makes sure we can create the site with this data
+        self.assertTrue(serializer.is_valid())
+
+    def test_display_serializer(self):
+        display_serializer = FeedSerializer(self.feed)
+
+        feed_data = display_serializer.data
+
+        self.assertEquals(feed_data['url'], self.feed_data['url'])
+        self.assertEquals(feed_data['missing_fields'], self.feed_data['missing_fields'])
+        self.assertEquals(feed_data['update_time'], self.feed_data['update_time'])
+        self.assertEquals(feed_data['source_site'], self.feed_data['source_site'])
 
 
 
