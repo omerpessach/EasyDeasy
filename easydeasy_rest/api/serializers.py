6a4bf3a693923f2c870255aa9a203c7d3de90b70
Paz <yolobazak1@gmail.com>
2021-04-27 16:32:42 +0300

    Updated to handle many field + fixed a problem where he'd try to validate already validated data.
    Modified the article paths usage to pathlib Path object to work on every OS.

--- easydeasy_rest/api/serializers.py
+++ easydeasy_rest/api/serializers.py
@@ -4,15 +4,30 @@ from easydeasy_rest import settings
 import os
 import random
 from typing import Type
+from pathlib import Path
 
 
-def handle_pk_or_str_post(validated_data: dict, value_key: str, model: Type[Model], serializer_key='name') -> dict:
+def handle_pk_or_str_post(validated_data: dict, value_key: str, model: Type[Model], serializer_key='name', many=False):
     """
     Checks if the validated data wanted value is pk/different key and modifies the validated data to what's needed!
     """
     value = validated_data[value_key]
-    kwargs = {'pk': int(value)} if value.isnumeric() else {serializer_key: value}
-    validated_data[value_key] = model.objects.get(**kwargs)
+
+    already_validated = many and all(type(item) is not str for item in value) or not many and type(value) is not str
+
+    if already_validated:
+        return validated_data
+
+    if many:
+        kwargs_list = [{'pk': int(instance_value)} for instance_value in value] \
+            if all(item.isnumeric() for item in value) \
+            else [{serializer_key: instance_value} for instance_value in value]
+
+        validated_data[value_key] = [model.objects.get(kwargs) for kwargs in kwargs_list]
+
+    else:
+        kwargs = {'pk': int(value)} if value.isnumeric() else {serializer_key: value}
+        validated_data[value_key] = model.objects.get(**kwargs)
 
     return validated_data
 
@@ -56,21 +71,23 @@ class SiteSerializer(ModelSerializer):
 class ArticleSerializer(ModelSerializer):
 
     def create(self, validated_data):
+        validated_data = handle_pk_or_str_post(validated_data, 'source_site', Site)
+        validated_data = handle_pk_or_str_post(validated_data, 'diseases', Disease, many=True)
 
         # Updates the to generic image if possible
         if validated_data['img'] is None:
             # Gets First Disease in list
             disease: Disease = validated_data['diseases'][0]
 
-            image_folder = f'/images/{disease.category.name}/'
-            folder_path = f'{settings.MEDIA_ROOT}{image_folder}'
+            image_folder = Path(f'/images/{disease.category.name}/')
+            folder_path = Path(f'{settings.MEDIA_ROOT}{image_folder}')
 
             # Selects random image
             random_image = random.choice(os.listdir(folder_path))
 
-            validated_data['img'] = f'{image_folder}{random_image}'
+            validated_data['img'] = str(image_folder / random_image)
 
-        return super(ArticleSerializer, self).create(validated_data)
+        return super().create(validated_data)
 
     class Meta:
         model = Article
